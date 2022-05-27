d457e4b4e3d3488667a9a316d726b4273a989c14
Iroennys <100889237+Iroeli@users.noreply.github.com>
2022-05-27 18:18:19 -0400

    Add files via upload

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "a86730aab5c59953c424abb4396d32d5"
 api_id = 14681595
-bot_token = "5232913510:AAHXoUTfMxxmWXPjPUkO1ZGsL5THrreWaHM"
-bot_admin_group = -1001640252635
+bot_token = "5232913510:AAGVU7EbQVb1xLyGt5PxwhR9gmr84Fl-E8c"
+bot_admin_group = -1001634617736
